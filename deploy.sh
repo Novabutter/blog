4c7c3dd41cd5a287fbb36765db10ef3add8a12b6
Giovanni <Novabutter>
2020-05-15 15:31:25 -0500

    Found Critical Error in deploy.sh, trying now

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
-#!/bin/bashecho -e "\033[0;32mDeploying updates to GitHub...\033[0m"cd publicif [ -n "$GITHUB_TOKEN" ]
+#!/bin/bash
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"cd publicif [ -n "$GITHUB_AUTH_SECRET" ]
 then
     touch ~/.git-credentials
     chmod 0600 ~/.git-credentials
-    echo $GITHUB_TOKEN > ~/.git-credentials    git config credential.helper store
+    echo $GITHUB_AUTH_SECRET > ~/.git-credentials    git config credential.helper store
     git config user.email "novabutter-blog-bot@nusers.no.reploy.github.com"
     git config user.name "novabutter-blog-bot"
 figit add .
