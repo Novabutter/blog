a2a3953157cfbdbb2304dcf8f8f01171f44dbde7
Giovanni <Novabutter>
2020-05-15 15:13:44 -0500

    Removal os sudo and deploy email correction

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ then
     touch ~/.git-credentials
     chmod 0600 ~/.git-credentials
     echo $GITHUB_TOKEN > ~/.git-credentials    git config credential.helper store
-    git config user.email "glicameli+blog-bot@outlook.com"
+    git config user.email "novabutter-blog-bot@nusers.no.reploy.github.com"
     git config user.name "novabutter-blog-bot"
 figit add .
 git commit -m "Rebuild site"
